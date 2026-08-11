3ba29f561a828d831e7f7efac0a6a7d6e6a6bfc5
PaskoS55 <pasko_sergey@yahoo.com>
2026-08-11 21:11:40 +0300

    fix sidebar link: /analytics/trends -> /analytics

--- apps/web/components/layout/Sidebar.tsx
+++ apps/web/components/layout/Sidebar.tsx
@@ -56,7 +56,7 @@ const navItems: NavItem[] = [
     ),
   },
   {
-    href: '/analytics/trends',
+    href: '/analytics',
     label: 'Динамика',
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
@@ -66,7 +66,7 @@ const navItems: NavItem[] = [
     ),
   },
   {
-    href: '/analytics/compare',
+    href: '/compare',
     label: 'Сравнение',
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
